f5e98273cbbd669a59e0b6aff014dcdbded3017d
pberton <1789738+pberton@users.noreply.github.com>
2024-07-16 19:04:08 -0700

    add formulas for omnistrate ctl

---
+++ Formula/omnistrate-ctl.rb
@@ -0,0 +1,32 @@
+class OmnistrateCTL < Formula
+    desc "Omnistrate CTL command line tool"
+    homepage "https://omnistrate.com"
+    version "1.0.0"
+  
+    if OS.mac?
+      if Hardware::CPU.intel?
+        url "https://github.com/omnistrate/cli/releases/#{version}/download/omnistrate-ctl-darwin-amd64"
+        sha256 "your_mac_intel_sha256"
+      elsif Hardware::CPU.arm?
+        url "https://github.com/omnistrate/cli/releases/#{version}/download/omnistrate-ctl-darwin-arm64"
+        sha256 "your_mac_arm_sha256"
+      end
+    elsif OS.linux?
+      if Hardware::CPU.intel?
+        url "https://github.com/omnistrate/cli/releases/#{version}/download/omnistrate-ctl-linux-amd64"
+        sha256 "your_linux_intel_sha256"
+      elsif Hardware::CPU.arm?
+        url "https://github.com/omnistrate/cli/releases/#{version}/download/omnistrate-ctl-linux-arm64"
+        sha256 "your_linux_arm_sha256"
+      end
+    end
+  
+    def install
+      bin.install "omnistrate-ctl"
+    end
+  
+    test do
+      system "#{bin}/omnistrate-ctl", "--version"
+    end
+  end
+  
