4274ac70736786041bebc03f98368fb0953cb0f4
pberton <1789738+pberton@users.noreply.github.com>
2024-07-17 21:56:51 -0700

    update sha

--- Formula/omnistrate-ctl.rb
+++ Formula/omnistrate-ctl.rb
@@ -6,18 +6,18 @@ class OmnistrateCtl < Formula
     if OS.mac?
       if Hardware::CPU.intel?
         url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-darwin-amd64"
-        sha256 "your_mac_intel_sha256"
+        sha256 "93e38e8d56396fc700752bd489895a8b5f9be8dd018279cdec314eace69e8d48"
       elsif Hardware::CPU.arm?
         url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-darwin-arm64"
-        sha256 "your_mac_arm_sha256"
+        sha256 "77168ac59b7cbe7f618505e6a0a2727b2451b3cd47300619cd58ff201dd5f475"
       end
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-linux-amd64"
-        sha256 "your_linux_intel_sha256"
+        sha256 "e9e97949d274bd9153c889fbce17e6a0b49803f9828b7ae4260ce2bc4536f22b"
       elsif Hardware::CPU.arm?
         url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-linux-arm64"
-        sha256 "your_linux_arm_sha256"
+        sha256 "241f44721db2595664c2dad07adb9dde10d2d6982b491bbe31aa9aca0a4ebe45"
       end
     end
   
