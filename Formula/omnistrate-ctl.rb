aac7233d5d4f79167c1595adee64a8f8b864965a
pberton <1789738+pberton@users.noreply.github.com>
2024-07-17 21:51:57 -0700

    update link

--- Formula/omnistrate-ctl.rb
+++ Formula/omnistrate-ctl.rb
@@ -1,22 +1,22 @@
 class OmnistrateCtl < Formula
     desc "Omnistrate CTL command line tool"
     homepage "https://omnistrate.com"
-    version "1.0.0"
+    version "v0.9.8"
   
     if OS.mac?
       if Hardware::CPU.intel?
-        url "https://github.com/omnistrate/cli/releases/#{version}/download/omnistrate-ctl-darwin-amd64"
+        url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-darwin-amd64"
         sha256 "your_mac_intel_sha256"
       elsif Hardware::CPU.arm?
-        url "https://github.com/omnistrate/cli/releases/#{version}/download/omnistrate-ctl-darwin-arm64"
+        url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-darwin-arm64"
         sha256 "your_mac_arm_sha256"
       end
     elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/omnistrate/cli/releases/#{version}/download/omnistrate-ctl-linux-amd64"
+        url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-linux-amd64"
         sha256 "your_linux_intel_sha256"
       elsif Hardware::CPU.arm?
-        url "https://github.com/omnistrate/cli/releases/#{version}/download/omnistrate-ctl-linux-arm64"
+        url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-linux-arm64"
         sha256 "your_linux_arm_sha256"
       end
     end
