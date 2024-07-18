f5821ca5d64d8e7308b10c4801e61e58e7833c23
pberton <1789738+pberton@users.noreply.github.com>
2024-07-18 07:31:58 +0000

    Update Omnistrate CTL Formula to 74\/merge
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/omnistrate-ctl.rb
+++ Formula/omnistrate-ctl.rb
@@ -1,12 +1,12 @@
 class OmnistrateCtl < Formula
     desc "Omnistrate CTL command line tool"
     homepage "https://omnistrate.com"
-    version "v0.9.8"
+    version "74/merge"
     
-    sha_darwin_amd64 = "93e38e8d56396fc700752bd489895a8b5f9be8dd018279cdec314eace69e8d48"
-    sha_darwin_arm64 = "77168ac59b7cbe7f618505e6a0a2727b2451b3cd47300619cd58ff201dd5f475"
-    sha_linux_amd64 = "e9e97949d274bd9153c889fbce17e6a0b49803f9828b7ae4260ce2bc4536f22b"
-    sha_linux_arm64 = "241f44721db2595664c2dad07adb9dde10d2d6982b491bbe31aa9aca0a4ebe45"
+    sha_darwin_amd64 = "32e14d805c074b1459c37dee353c1ed4f7beedd1f2b7ad1d7ef9e8b6d339f392"
+    sha_darwin_arm64 = "ba5e25c4abe565a4e9223d33479f5a96133c82285930691e7f5cbf8290c6acf5"
+    sha_linux_amd64 = "7db2e0185edc44492aa4e4d81bb80aaf17b12f482859c32cac3ea2c7b74733fc"
+    sha_linux_arm64 = "d616875151a45cfc60118aaeca5ebb3f06c8a42954a33d9302d79dd131014b62"
 
     if OS.mac?
       if Hardware::CPU.intel?
