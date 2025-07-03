9fb42007efe05c206e33887d31c142fefeeead81
pberton <1789738+pberton@users.noreply.github.com>
2025-07-03 12:36:22 -0700

    update logic to use the new compressed files (#80)

--- Formula/omnistrate-ctl.rb
+++ Formula/omnistrate-ctl.rb
@@ -3,43 +3,42 @@ class OmnistrateCtl < Formula
     homepage "https://omnistrate.com"
     version "v0.13.29"
     
-    sha_darwin_amd64 = "f3ef73b1d894ff56adc224412c5d309ef7662b1d90c61185391fe055935f3bca"
-    sha_darwin_arm64 = "65a3dbc3653d4e3953453be6eb6590a3481f94921b1097f4feb8fa09e90d6f07"
-    sha_linux_amd64 = "dcc358bd02a369a4fc70dea577b6ea4debddce3e3bd912db7ff5596646f5385e"
-    sha_linux_arm64 = "4005981ac5a76b23e4a895cc2e6f63ba352422c397b0cdd5dcc28bff06c83cc2"
+    sha_darwin_amd64 = "97d71d5226f6241eae9efcddcf54b8fb4260ba707c6abfd91e474c545a8e8707"
+    sha_darwin_arm64 = "271a7b17dbdd69ada7089ea0f37f2ad6d7cbcebc9e00f4df4721bd61567aa767"
+    sha_linux_amd64 = "3b2ee8035912d3abb9f4b39a81b334f51acc3056151e9a4b553b623e811c3de5"
+    sha_linux_arm64 = "4cc770d130b94b2f9d19bcbb1f1d0aba53efbcaa12b8e7b0ba759adc59f1d78c"
 
     if OS.mac?
       if Hardware::CPU.intel?
-        url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-darwin-amd64"
+        url "https://github.com/omnistrate-oss/omnistrate-ctl/releases/download/#{version}/omnistrate-ctl-darwin-amd64.tar.gz"
         sha256 sha_darwin_amd64
       elsif Hardware::CPU.arm?
-        url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-darwin-arm64"
+        url "https://github.com/omnistrate-oss/omnistrate-ctl/releases/download/#{version}/omnistrate-ctl-darwin-arm64.tar.gz"
         sha256 sha_darwin_arm64
       end
     elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-linux-amd64"
+        url "https://github.com/omnistrate-oss/omnistrate-ctl/releases/download/#{version}/omnistrate-ctl-linux-amd64.tar.gz"
         sha256 sha_linux_amd64
       elsif Hardware::CPU.arm?
-        url "https://github.com/omnistrate/cli/releases/download/#{version}/omnistrate-ctl-linux-arm64"
+        url "https://github.com/omnistrate-oss/omnistrate-ctl/releases/download/#{version}/omnistrate-ctl-linux-arm64.tar.gz"
         sha256 sha_linux_arm64
       end
     end
   
     def install
-        if OS.mac?
-            if Hardware::CPU.intel?
-                bin.install "omnistrate-ctl-darwin-amd64" => "omnistrate-ctl"
-            elsif Hardware::CPU.arm?
-                bin.install "omnistrate-ctl-darwin-arm64" => "omnistrate-ctl"
-            end
-        elsif OS.linux?
-            if Hardware::CPU.intel?
-                bin.install "omnistrate-ctl-linux-amd64" => "omnistrate-ctl"
-            elsif Hardware::CPU.arm?
-                bin.install "omnistrate-ctl-linux-arm64" => "omnistrate-ctl"
-            end
-        end
+        # Homebrew automatically extracts .tar.gz files
+        # The extracted binary will be available in the current directory
+        
+        # Determine the binary name based on platform
+        binary_name = if OS.mac?
+                        Hardware::CPU.intel? ? "omnistrate-ctl-darwin-amd64" : "omnistrate-ctl-darwin-arm64"
+                      elsif OS.linux?
+                        Hardware::CPU.intel? ? "omnistrate-ctl-linux-amd64" : "omnistrate-ctl-linux-arm64"
+                      end
+        
+        # Install the binary
+        bin.install binary_name => "omnistrate-ctl"
         bin.install_symlink "omnistrate-ctl" => "omctl"
     end
   
