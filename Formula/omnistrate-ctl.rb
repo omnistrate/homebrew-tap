41c0296b73d89523faadd8f579a501b27f4e9d3a
pberton <1789738+pberton@users.noreply.github.com>
2025-09-12 12:16:50 -0700

    Update Omnistrate CTL Formula to v0.14.61 (#110)
    This PR updates the Omnistrate CTL Formula to version v0.14.61.
    The SHA256 checksums have been updated as well.
    Once the PR is merged, the new version will be available in the
    Omnistrate Homebrew Tap.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: aloknnikhil <8571509+aloknnikhil@users.noreply.github.com>

--- Formula/omnistrate-ctl.rb
+++ Formula/omnistrate-ctl.rb
@@ -1,12 +1,12 @@
 class OmnistrateCtl < Formula
     desc "Omnistrate CTL command line tool"
     homepage "https://omnistrate.com"
-    version "v0.14.60"
+    version "v0.14.61"
     
-    sha_darwin_amd64 = "93dc341594eaa41ae97784e2ee2296524e45a0f1c65b399d8bfe4f52546d75fc"
-    sha_darwin_arm64 = "d879868c2858a84bd5a22964e8b4a65c2d4f24fffbb6839545a74bfc0eb52887"
-    sha_linux_amd64 = "f799902cba8aaaa92fe7ce0d3f9c15e3ceaf26965b0620bb925a81dde8843ee1"
-    sha_linux_arm64 = "6943f9a271c042b02c54fa9f8807cff594874062a61301f669aac8d38c9764e7"
+    sha_darwin_amd64 = "c50748762a12b84b2983e9a398be1bba20eb75619432ee40bb0507f92bdf2aeb"
+    sha_darwin_arm64 = "d10ef93286e5509e060b9c9b1ddfd942de0a38145efe7107c0a708e7679ce1ca"
+    sha_linux_amd64 = "05b1739c1585e3d9a3a011deb81087a4c553e9b17af33d1f3c4bef32350be95e"
+    sha_linux_arm64 = "f4f82bbc7bb5c04a61d6c5be709b922ab800cc32ec6bca8d56dedcd571161d61"
 
     if OS.mac?
       if Hardware::CPU.intel?
